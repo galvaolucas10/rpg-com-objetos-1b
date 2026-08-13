e6182069f2e263cc13a9f4375d82c2fdee48122d
galvaolucas10 <galvaolucas943@gmail.com>
2026-08-13 07:48:24 -0300

    teste classe

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 class Personagem {
     constructor(nome, vida, ataque) {
-        this.nome = nome,
-            this.vida = vida,
-            this.ataque = ataque
+        this.nome = nome
+        this.vida = vida
+        this.ataque = ataque
     }
 
     causaDano(personagem) {
-        personagemano(this.ataque)
+        personagem.recebeDano(this.ataque)
     }
 
     recebeDano(quantidade) {
@@ -18,9 +18,10 @@ class Personagem {
     }
 }
 
-const druida = new Personagem("Obin", 150, 15)
-const guerreiro = new Personagem("Thorin", 60, 20)
-const mago = new Personagem("Gandalfe", 60, 35)
-const arqueiro = new Personagem("Legolas", 80, 25)
-
-console.log(druida)
+const druida = new personagem("Obin", 55, 35)
+const guerreiro = new personagem("Thorin", 60, 20)
+const mago = new personagem("Gandalfe", 60, 35)
+const arqueiro = new personagem("Legolas", 80, 25)
+const ladrao = new personagem("Sombra", 50, 40)
+const paladino = new personagem("Uther", 90, 15)
+const necromante = new personagem("Malakor", 55, 35)
