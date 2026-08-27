14383c75ae957d8f3c514446e941ed17afcff935
galvaolucas10 <galvaolucas943@gmail.com>
2026-08-27 07:37:38 -0300

    classes

--- app.js
+++ app.js
@@ -16,12 +16,25 @@ class Personagem {
             this.vida = "morreu"
         }
     }
+
+    estaVivo(){
+        return this.vida > 0
+    }
+    mostrarStatus(){
+        console.log(this.nome + " | Vida: " + this.vida)
+    }
 }
 
-const druida = new personagem("Obin", 55, 35)
-const guerreiro = new personagem("Thorin", 60, 20)
-const mago = new personagem("Gandalfe", 60, 35)
-const arqueiro = new personagem("Legolas", 80, 25)
-const ladrao = new personagem("Sombra", 50, 40)
-const paladino = new personagem("Uther", 90, 15)
-const necromante = new personagem("Malakor", 55, 35)
+const druida = new Personagem("Obin", 55, 35)
+const guerreiro = new Personagem("Thorin", 60, 20)
+const mago = new Personagem("Gandalfe", 60, 35)
+const arqueiro = new Personagem("Legolas", 80, 25)
+const ladrao = new Personagem("Sombra", 50, 40)
+const paladino = new Personagem("Uther", 90, 15)
+const necromante = new Personagem("Malakor", 55, 35)
+
+mago.causaDano(arqueiro)
+mago.causaDano(arqueiro)
+druida.causaDano(arqueiro)
+arqueiro.mostrarStatus()
+console.log(arqueiro.estaVivo())
